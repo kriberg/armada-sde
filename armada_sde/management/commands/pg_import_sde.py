6a161bdfcadd62d3e468d77c498246269bc6b5b0
Kristian Berg <kriberg@tihlde.org>
2018-10-02 22:55:30 +0200

    Split load_sde into separate parts so stock models can be used

---
+++ armada_sde/management/commands/pg_import_sde.py
@@ -0,0 +1,99 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+import shlex
+import sys
+from subprocess import PIPE, Popen
+from django.conf import settings
+from django.db import connections
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = ('(Re)Loads a new SDE dump from Fuzzysteve\'s SDE dump site into '
+            'the database.')
+    requires_system_checks = False
+
+    def add_arguments(self, parser):
+        parser.add_argument('-d', '--dbalias', action='store', dest='database',
+                            default='default',
+                            help='Database alias defined in your project '
+                                 'settings to load the SDE into. Defaults '
+                                 'to \'default\'')
+        parser.add_argument('-f', '--file', action='store', dest='file',
+                            help='Fuzzysteve\'s postgres-schemas-latest dump '
+                                 'file to use')
+        parser.add_argument('-s', '--skip-import', action='store_true',
+                            dest='skip',
+                            help='Skip importing the dump, just rename tables')
+
+    def import_database(self, dump, dbalias):  # pragma: no cover
+        connection = connections[dbalias]
+        database = settings.DATABASES[dbalias]
+
+        # Create the yaml user first, to supress some common errors. Then get
+        # rid of any old SDE data.
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1 FROM pg_roles WHERE rolname='yaml';")
+            row = cursor.fetchone()
+            try:
+                row[0] == 1
+            except (IndexError, TypeError):
+                self.stdout.write('Creating the yaml user in the database...')
+                cursor.execute('CREATE USER yaml')
+                self.stdout.write('[DONE]\n')
+
+            self.stdout.write('Dropping the evesde schema, if exists...')
+            cursor.execute("DROP SCHEMA IF EXISTS evesde CASCADE;")
+            self.stdout.write('[DONE]\n')
+
+        params = ["pg_restore",
+                  "-d",
+                  database['NAME']]
+        if database['USER']:
+            params.append('-U')
+            params.append(database['USER'])
+        params.append(dump)
+
+        sys.stdout.write('Starting import of database dump...')
+        sys.stdout.flush()
+        p = Popen(shlex.split(" ".join(params)),
+                  stdin=PIPE,
+                  stdout=PIPE,
+                  stderr=PIPE,
+                  shell=False)
+        returncode = p.wait()
+        if returncode != 0:
+            sys.stdout.write('[FAILED]\n')
+            self.stdout.write(p.stdout.read())
+            self.stdout.flush()
+            self.stderr.write(p.stderr.read())
+            self.stderr.flush()
+            sys.exit(returncode)
+        sys.stdout.write('[DONE]\n')
+
+    def rename_tables(self, dbalias):
+        connection = connections[dbalias]
+        with connection.cursor() as cursor:
+            cursor.execute('SET search_path TO evesde;')
+            ispect = connection.introspection
+            tables_to_introspect = ispect.table_names(cursor)
+
+            for table_name in tables_to_introspect:
+                renamed_table_name = 'sde_{}'.format(table_name)
+                cursor.execute('DROP TABLE IF EXISTS evesde."{}";'.format(
+                    renamed_table_name))
+                cursor.execute('ALTER TABLE evesde."{}" '
+                               'RENAME TO "{}";'.format(
+                    table_name,
+                    renamed_table_name))
+                sys.stdout.write('Renamed {} to {}'.format(table_name,
+                                                           renamed_table_name))
+
+    def handle(self, *args, **options):
+        dbalias = options['database']
+        dump = options.get('file')
+        skip = options.get('skip')
+
+        if not skip and dump:
+            self.import_database(dump, dbalias)
+        self.rename_tables(dbalias)
--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
-from django.test import TestCase, override_settings
+from django.test import TransactionTestCase, override_settings
 from django.test.utils import captured_stdout
 from django.db import DEFAULT_DB_ALIAS
 from django.core.management import call_command
 from . import run_sql
 
 
-class LoadSDECommandTests(TestCase):
+class LoadSDECommandTests(TransactionTestCase):
     def setUp(self):
         self.project_root = os.path.join('tests', 'testapp')
         self._settings = os.environ.get('DJANGO_SETTINGS_MODULE')
@@ -22,19 +22,35 @@ class LoadSDECommandTests(TestCase):
         if self._settings:
             os.environ['DJANGO_SETTINGS_MODULE'] = self._settings
 
-    def test_load_sde_command(self):
+    def test_import_sde_command(self):
         with captured_stdout() as stdout:
-            call_command('pg_load_sde')
+            call_command('pg_import_sde', '-s')
         output = stdout.getvalue()
-        self.assertIn('# Generated', output)
-        self.assertIn('class InvType(models.Model)', output)
+        self.assertIn('Renamed invTypes to sde_invTypes', output, msg=output)
+
+    def test_generate_models_command(self):
+        run_sql(DEFAULT_DB_ALIAS,
+                os.path.join(self.project_root,
+                             'fixtures',
+                             'rename_tables.sql'))
+        with captured_stdout() as stdout:
+            call_command('pg_generate_models')
+        output = stdout.getvalue()
+        self.assertIn('# Generated', output, msg=output)
+        self.assertIn('class InvType(models.Model)', output, msg=output)
+        self.assertIn('db_table = \'sde_invTypes\'', output, msg=output)
 
     @override_settings(ARMADA={'SDE': {'schema': 'sde'}})
-    def test_load_sde_to_custom_schema(self):
+    def test_move_sde_command(self):
+        run_sql(DEFAULT_DB_ALIAS,
+                os.path.join(self.project_root,
+                             'fixtures',
+                             'rename_tables.sql'))
         with captured_stdout() as stdout:
-            call_command('pg_load_sde')
+            call_command('pg_move_sde')
         output = stdout.getvalue()
-        self.assertIn('Moving SDE tables to sde schema...', output)
-        self.assertIn('Moving evesde.sde_invTypes -> sde.sde_invTypes', output)
+        self.assertIn('Moving SDE tables to sde schema...', output, msg=output)
+        self.assertIn('Moving evesde.sde_invTypes -> sde.sde_invTypes', output,
+                      msg=output)
         self.assertIn('Adding index for primary key column to table '
-                      'sde_industryActivityMaterials', output)
+                      'sde_industryActivityMaterials', output, msg=output)
